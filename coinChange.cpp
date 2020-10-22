27ee534dac8b475cebe4db0e83f895e1a3713a91
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-22 12:53:57 +0600

    need to sort all the coins first

--- coinChange.cpp
+++ coinChange.cpp
@@ -8,13 +8,26 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>coin[i];
     }
+
+    int flag = 0;
+    for(int i=0;i<n-1;i++){                         //Need to Sort first
+        for(int j=0;j<n-i-1;j++){
+            if(coin[j]<coin[j+1]){
+                int temp = coin[j];
+                coin[j] = coin[j+1];
+                coin[j+1] = temp;
+                flag = 1;
+            }
+        }
+        if(flag = 0) break;
+    }
+
     cout<<endl<<"What amount you want to change? : "; cin>>amount;
     int i = 0;
     while(amount>0){
         int value = amount/coin[i];
         change[i] = value;
         amount = amount - coin[i]*value;
-        cout<<"hi";
         i++;
     }
     for(int i=0;i<n;i++){
