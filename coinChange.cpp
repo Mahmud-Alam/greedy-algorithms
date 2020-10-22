ebdf16f29568477713024f724020cfb92b5ec7d0
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-22 12:46:08 +0600

    done for only few cases(infinite loop getting error bcz limited index number n)

--- coinChange.cpp
+++ coinChange.cpp
@@ -1,3 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int main(){
+    int amount,n,coin[100],change[100];
+    cout<<"How many types of coin you have? : "; cin>>n;
+    cout<<endl<<"Input value of coins";
+    for(int i=0;i<n;i++){
+        cin>>coin[i];
+    }
+    cout<<endl<<"What amount you want to change? : "; cin>>amount;
+    int i = 0;
+    while(amount>0){
+        int value = amount/coin[i];
+        change[i] = value;
+        amount = amount - coin[i]*value;
+        cout<<"hi";
+        i++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<change[i]<<"   ";
+    }
+}
