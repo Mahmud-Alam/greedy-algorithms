2de61db79e32596c7892418e8e3b412482cae894
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-22 23:59:04 +0600

    user input

--- jobSequencing.cpp
+++ jobSequencing.cpp
@@ -6,3 +6,16 @@ class Job{
     int deadLine;
     int profit;
 };
+
+int main(){
+    Job arr[10];
+    int n;
+    cout<<"Input Job's amount number: ";cin>>n;
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<"Input "<<i+1<<"'s Job ID no   : ";cin>>arr.id;
+        cout<<"Input "<<i+1<<"'s Job DeadLine: ";cin>>arr.id;
+        cout<<"Input "<<i+1<<"'s Job Profit  : ";cin>>arr.id;
+        cout<<endl;
+    }
+}
