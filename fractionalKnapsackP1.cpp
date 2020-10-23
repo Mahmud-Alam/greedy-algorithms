6358fa878035d1000767acc059b915d5596dab76
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-23 14:27:19 +0600

    struct node and main

--- fractionalKnapsackP1.cpp
+++ fractionalKnapsackP1.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+struct Node{
+    int weight;
+    int value;
+    double ratio;
+} item[100];
+
+int main(){
+    int n;
+    cout<<"Input a number: ";cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<endl<<"input "<<i+1<<" item's weight: ";cin>>item[i].weight;
+        cout<<"input "<<i+1<<" item's value : ";cin>>item[i].value;
+        item[i].ratio = (double)item[i].value/item[i].weight;
+    }
+}
