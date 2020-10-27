6145cc7f019ac0695e2a6ce0cf60b8eae304fe15
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-27 23:53:48 +0600

    -

--- jobSequencing.cpp
+++ jobSequencing.cpp
@@ -1,21 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Job{
+class Node{
+    public:
     char id;
     int deadLine;
     int profit;
 };
 
+void jobSequencing(int n, Node selection[], Node job[]){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(job[j].profit<job[j].profit){
+                Node temp = job[j];
+                job[j] = job[j+1];
+                job[j+1] = temp;
+            }
+        }
+    }
+}
+
 int main(){
-    Job arr[10];
+    Node job[10], selection[10];
     int n;
     cout<<"Input Job's amount number: ";cin>>n;
     cout<<endl;
     for(int i=0;i<n;i++){
-        cout<<"Input "<<i+1<<"'s Job ID no   : ";cin>>arr.id;
-        cout<<"Input "<<i+1<<"'s Job DeadLine: ";cin>>arr.id;
-        cout<<"Input "<<i+1<<"'s Job Profit  : ";cin>>arr.id;
+        cout<<"Input "<<i+1<<"'s Job ID no   : ";cin>>job[i].id;
+        cout<<"Input "<<i+1<<"'s Job DeadLine: ";cin>>job[i].deadLine;
+        cout<<"Input "<<i+1<<"'s Job Profit  : ";cin>>job[i].profit;
         cout<<endl;
     }
+
+    jobSequencing(n,selection,job);
 }
