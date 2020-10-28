e1cfbb724e5b4bb66c1bd18be387d866f5a4e387
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-28 18:10:33 +0600

    sort arrival time ascending order

--- minimumNumberOfPlatforms.cpp
+++ minimumNumberOfPlatforms.cpp
@@ -8,11 +8,20 @@ class Node{
 };
 
 int main(){
-    int n;
+    int n,count=1;
     Node time[100];
     cout<<"Input a number of n: ";cin>>n;
     for(int i=0;i<n;i++){
         cout<<endl<<"Input "<<i+1<<"'s Arrival time  : ";cin>>time[i].arr;
         cout<<"Input "<<i+1<<"'s Departure time: ";cin>>time[i].dep;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(time[j].arr>time[j+1].arr){
+                Node temp = time[j];
+                time[j] = time[j+1];
+                time[j+1] = temp;
+            }
+        }
     }
 }
