1350eb6319388db016ea1b0f8de7478d2edb2cf3
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-28 10:35:31 +0600

    function complete

--- jobSequencing.cpp
+++ jobSequencing.cpp
@@ -6,6 +6,7 @@ class Node{
     char id;
     int deadLine;
     int profit;
+    bool flag = false;
 };
 
 void jobSequencing(int n, Node selection[], Node job[]){
@@ -19,11 +20,26 @@ void jobSequencing(int n, Node selection[], Node job[]){
         }
     }
 
+//    for(int i=0;i<n;i++){
+//        cout<<job[i].id<<"   ";
+//        cout<<job[i].deadLine<<"   ";
+//        cout<<job[i].profit<<"   ";
+//        cout<<endl;
+//    }
+
     for(int i=0;i<n;i++){
-        cout<<job[i].id<<"   ";
-        cout<<job[i].deadLine<<"   ";
-        cout<<job[i].profit<<"   ";
-        cout<<endl;
+        for(int j=n-1;j>=0;j--){
+            if(job[i].deadLine>=j+1&&selection[j].flag==false){
+                selection[j] = job[i];
+                break;
+            }
+        }
+    }
+    int z=0;
+    while(selection[z].flag!=false){
+        cout<<selection[z].id<<"\t";
+        cout<<selection[z].profit<<endl;
+        z++;
     }
 }
 
@@ -36,6 +52,7 @@ int main(){
         cout<<"Input "<<i+1<<"'s Job ID no   : ";cin>>job[i].id;
         cout<<"Input "<<i+1<<"'s Job DeadLine: ";cin>>job[i].deadLine;
         cout<<"Input "<<i+1<<"'s Job Profit  : ";cin>>job[i].profit;
+        job[i].flag = true;
         cout<<endl;
     }
 
