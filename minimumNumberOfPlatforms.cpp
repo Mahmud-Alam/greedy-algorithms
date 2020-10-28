8bc9c17fb988c38c7df4dc3d58632d0f7a73d2e2
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-28 18:00:29 +0600

    user input of arrival time and departure time

--- minimumNumberOfPlatforms.cpp
+++ minimumNumberOfPlatforms.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 class Node{
+    public:
     int arr;
     int dep;
 };
+
+int main(){
+    int n;
+    Node time[100];
+    cout<<"Input a number of n: ";cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<endl<<"Input "<<i+1<<"'s Arrival time  : ";cin>>time[i].arr;
+        cout<<"Input "<<i+1<<"'s Departure time: ";cin>>time[i].dep;
+    }
+}
