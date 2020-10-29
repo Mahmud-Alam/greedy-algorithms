53bfd3212cc7cfa7e25e62280f0fa9e98ca76845
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-29 15:17:10 +0600

    done

--- minimumOperationsToMakeArrayEqual.cpp
+++ minimumOperationsToMakeArrayEqual.cpp
@@ -7,22 +7,33 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr1[i];
         t1[i] = arr1[i];
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
         cin>>arr2[i];
         t2[i] = arr2[i];
-        cout<<endl;
     }
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(arr1[j]>arr1[j+1]){
-                int temp = arr1[j];
-                arr1[j] = arr1[j+1];
-                arr1[j+1] = temp;
+            if(t1[j]>t1[j+1]){
+                int temp = t1[j];
+                t1[j] = t1[j+1];
+                t1[j+1] = temp;
             }
-            if(arr2[j]>arr2[j+1]){
-                int temp = arr2[j];
-                arr2[j] = arr2[j+1];
-                arr2[j+1] = temp;
+            if(t2[j]>t2[j+1]){
+                int temp = t2[j];
+                t2[j] = t2[j+1];
+                t2[j+1] = temp;
             }
         }
     }
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(t1[i]!=t2[i]){
+            int result = t1[i] - t2[i];
+            if(result<0) result*=-1;
+            count+=result;
+        }
+    }
+    cout<<endl<<"Minimum Operations to make Array Equal is: "<<count<<endl;
 }
