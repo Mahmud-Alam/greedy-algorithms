d254f7ee38d5b9e39babca255b920d1769f2aa41
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-29 14:50:36 +0600

    minimum operations to make array equal

---
+++ minimumOperationsToMakeArrayEqual.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,arr1[100],arr2[100],t1[100],t2[100];
+    cout<<"Input array size: ";cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr1[i];
+        t1[i] = arr1[i];
+        cin>>arr2[i];
+        t2[i] = arr2[i];
+        cout<<endl;
+    }
+
+}
